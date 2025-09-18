7b0f21eaa2425d83dc9b85bceb3916b4d7fe71bf
R-Akila-2006 <akilaravikumar05@gmail.com>
2025-09-18 12:56:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log(1+1); // varible declaration
+let a = 10;
+let b= 1;
+
+console.log(a+b); 
+a= 11; // assign
+a=12 // Reassign
+
+// const name = "potter";
+// console.log(name);
+
+// var Name = "harry";
+// let msg = "I am potter";
+// // Name = "potter"
+
+// console.log(Name +  "   "+ msg)
+// // Let accept the reassign value but const can not accept the reassign value so const is best //
+
+// var  Name = "potter";
+// console.log(Name);
+// // var allow the reassign and redeclare so it is not use //
+
+const   Name = "HARRY POTTER"
+let msg ="I am potter";
+console.log(Name[2]+" "+msg)
+
+console.log(10 ==="10"); 
+// ==(double equcal) can not check the inside the content so the  output True
+// == check the inside the content so the output False
+
+if(!false){
+    console.log(true);
+}else{
+    console.log(false);
+}
+//0 is always  false and  other number is truthy value//
+
+let person; 
+if(null){
+    
+    console.log(true);
+}else{
+    console.log(false)
+}
+console.log(person);
+
+
+
+console.log("10" +"10Ak")
+console.log("10" -"10Ak")
+// Nan Not a number//
+
+if(NaN){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
